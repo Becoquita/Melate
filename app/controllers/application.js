5dcb16c3e73f35b00d57e0c47f33ed93060a62b8
Becoquita <A01271459@itesm.mx>
2017-11-21 01:05:44 -0500

    Melate

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -23,22 +23,23 @@ export default Controller.extend({
 			let PXW;
 			let PXL;
 			let ExpectedEarnings;
+      		let table=[];
 			//console.log('Sold Tickets | Expected earnings');
 			if (b1 === ''){
 				alert('The first ticket cannot be empty');
 				return;
 			} else {
-			if (b2!= undefined ){
-				debugger
+			if (b2 != undefined ){
+				
 				for (;b1<=b2; b1++){
 			    XW = b1*CostSale;
 					XL = b1*CostProduction;
 			    PXW = (XW-XL)*PW;
 					PXL = jackpot*PL;
 			    ExpectedEarnings = PXW-PXL;
-			    return b1 + '|' + ExpectedEarnings;
+			    table.push( '  ' + b1 + ' | ' + ExpectedEarnings + ' ');
 			    //console.log(B1+' | '+ExpectedEarnings);
-			    //console.log('_____________');
+			    //console.log('___');
 			  } 
 			} else {
 				  XW = b1*CostSale;
@@ -46,14 +47,31 @@ export default Controller.extend({
 			    PXW = (XW-XL)*PW;
 					PXL = jackpot*PL;
 			    ExpectedEarnings = PXW-PXL;
-			    return b1 + ' | ' + ExpectedEarnings;
+			    table.push( '  ' + b1 + ' | ' + ExpectedEarnings + ' ');
 			    //console.log(B1+' | '+ExpectedEarnings);	
 			}
+        return table;
 			}
 		}
 
 			let x = ganancias(B1, B2);
-			this.set('n', x);
+			//this.set('n', x);
+
+			var html = "<table style='width:400px'><thead><tr><th>Sold Tickets</th><th>Expected Earnings</th></tr></thead>";
+
+			for(var i=0;i<x.length;i++)
+			{
+				var obj = x[i].split("|");
+
+				html += "<tr>";
+				html += "<td>" + obj[0] + "</td><td>" + obj[1] + "</td>";
+				html += "</tr>";
+
+			}
+
+			html += "</table>";
+
+			document.getElementById("tabla").innerHTML = html;
 
 		},
 
@@ -61,13 +79,17 @@ export default Controller.extend({
 		clear(){
 			//dejamos las variables sin datos
 			this.setProperties({
-				B1:'',
-				B2:'',
+				B1: undefined,
+				B2: undefined,
 			});
 
 			//Vuelve a dejar todas las variables sin datos
 			this.set('n',undefined);
+
+			document.getElementById("tabla").innerHTML = " ";
 			}
+
+
 	}
 });
 
